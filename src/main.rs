debb6496e8bd0d4195d1142f942deba23f61ad78
Beomjun Kim <nutyworks@gmail.com>
2022-03-17 20:05:19 +0900

    Add help message

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 mod tictactoe;
 
 fn main() {
+    println!("You can type coordinate (e. g. A1, B3, C2) to make your turn.");
+    println!("^C (Ctrl-C) to exit.");
+    println!("");
+    println!("A1 B1 C1");
+    println!("A2 B2 C2");
+    println!("A3 B3 C3");
+
     tictactoe::start_game();
 }
